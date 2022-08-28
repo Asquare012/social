84efc83c08296c265b04f09e5f1fc34437d4728a
Hassan Abass <abass.hassan.7798@gmail.com>
2022-08-29 00:20:25 +0100

    Update social.js

--- social.js
+++ social.js
@@ -1,37 +1,37 @@
 // togg = () => {
 //     // body
 //     let body = document.body;
-//     body.classList.toggle("body");
-//     // header
-//     let head = document.querySelector("header");
-//     head.classList.toggle("head");
-//     // ball
-//     let ball = document.querySelector("#ball");
-//     ball.classList.toggle("ball");
+    body.classList.toggle("body");
+// header
+    let head = document.querySelector("header");
+    head.classList.toggle("head");
+     // ball
+    let ball = document.querySelector("#ball");
+    ball.classList.toggle("ball");
 
-//     // top box
-//     let a = document.querySelector("#two").querySelector("#first");
-//     a.classList.toggle("box");
+    // top box
+    let a = document.querySelector("#two").querySelector("#first");
+     a.classList.toggle("box");
 
-//     let b = document.querySelector("#two").querySelector("#second");
-//     b.classList.toggle("box");
+    let b = document.querySelector("#two").querySelector("#second");
+    b.classList.toggle("box");
 
-//     let c = document.querySelector("#two").querySelector("#third");
-//     c.classList.toggle("box");
+    let c = document.querySelector("#two").querySelector("#third");
+c.classList.toggle("box");
 
-//     let d = document.querySelector("#two").querySelector("#fourth");
-//     d.classList.toggle("box");
+   let d = document.querySelector("#two").querySelector("#fourth");
+   d.classList.toggle("box");
 
-//     //overview
-//     let overview = document.querySelector("#three");
-//     overview.classList.toggle("over");
+    //overview
+    let overview = document.querySelector("#three");
+    overview.classList.toggle("over");
 
-//     // last box
-//     let e = document.querySelector("#last").querySelector("#first");
-//     e.classList.toggle("box");
+    // last box
+    let e = document.querySelector("#last").querySelector("#first");
+    e.classList.toggle("box");
 
-//     let f = document.querySelector("#last").querySelector("#second");
-//     f.classList.toggle("box");
+   let f = document.querySelector("#last").querySelector("#second");
+   f.classList.toggle("box");
 
 //     let g = document.querySelector("#last").querySelector("#third");
 //     g.classList.toggle("box");
